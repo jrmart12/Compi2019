326f8ec5ae4ee2b9d57a4e3e22de8e9d3f82a58a
jose <rodolfojose@live.com>
2019-05-05 23:45:55 -0600

    weqe

--- prueba1 copy/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Desktop/Prueba1/Prueba1_P22019/tests
-# Build directory: /home/ubuntu/Desktop/Prueba1/Prueba1_P22019/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_lexer "/home/ubuntu/Desktop/Prueba1/Prueba1_P22019/tests/test_lexer")
